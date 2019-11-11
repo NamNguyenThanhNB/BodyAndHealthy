22919fe15a8c79cc9d2923295fb37a7cb5e696bd
NamNguyenThanhNB <50911569+NamNguyenThanhNB@users.noreply.github.com>
2019-11-11 21:40:34 +0700

    tạo các màn hình, chức năng cơ bản cho dự án

---
+++ app/src/main/java/com/thanhnam/bodyandhealthy/activity/ManHinhChaoActivity.java
@@ -0,0 +1,16 @@
+package com.thanhnam.bodyandhealthy.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thanhnam.bodyandhealthy.R;
+
+public class ManHinhChaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_chao);
+    }
+}
---
+++ app/src/main/java/com/thanhnam/bodyandhealthy/activity/TinhToanActivity.java
@@ -0,0 +1,16 @@
+package com.thanhnam.bodyandhealthy.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thanhnam.bodyandhealthy.R;
+
+public class TinhToanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tinh_toan);
+    }
+}
